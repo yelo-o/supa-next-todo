41f99b6e19a6d00f7a6a31bcbd9a8a830296d8dd
yelo-o <flyordig@gmail.com>
2024-07-13 10:25:56 +0900

    todoController - create, update, delete, searchbyId

--- app/todo-no-rls/hooks/useTodosController.tsx
+++ app/todo-no-rls/hooks/useTodosController.tsx
@@ -1,4 +1,4 @@
-import { getTodos } from '@/apis/todos-no-rls';
+import { createTodos, deleteTodosSoft, getTodos, getTodosBySearch, updateTodos } from '@/apis/todos-no-rls';
 import { Database } from '@/types/supabase';
 import { useState, useEffect } from 'react'
 
@@ -8,6 +8,7 @@ const useTodosController = () => {
     const [loading, setLoading] = useState(false);
     const [todos, setTodos] = useState<TodoDto[]>([]);
 
+    // todos 가져오기
     const onGetTodos = async () => {
         setLoading(true);
         try {
@@ -22,12 +23,41 @@ const useTodosController = () => {
         }
     };
 
-
     useEffect(() => {
         onGetTodos();
     }, []);
 
-    return { loading, todos };
+    // 비어있는 todo 생성하기
+    const onCreateEmptyTodos = async () => {
+        await createTodos ("");
+        await onGetTodos();
+    }
+
+    // todo 업데이트하기
+    const onUpdateTodos = async (id: number, content: string) => {
+        await updateTodos(id, content);
+        await onGetTodos();
+    }
+
+    // todo 삭제하기
+    const onDeleteTodos = async (id: number) => {
+        await deleteTodosSoft(id);
+        await onGetTodos();
+    }
+
+    // todo 검색하기
+    const onSearchTodos = async (terms: string) => {
+        if (terms) {
+            const todoResult = await getTodosBySearch(terms);
+            if (todoResult) {
+                setTodos(todoResult);
+            }
+        } else {
+            await onGetTodos();
+        }
+    }
+
+    return { loading, todos, onCreateEmptyTodos, onUpdateTodos, onDeleteTodos, onSearchTodos };
 };
 
 export default useTodosController;
