27944dafbbe69982dcc48751f49e9ff5402d7cfa
yelo-o <flyordig@gmail.com>
2024-07-18 23:36:20 +0900

    supabase 서버클라이언트

---
+++ lib/supabase.ts
@@ -0,0 +1,55 @@
+// RouteHandler, RSC, Middleware, ServerActions
+
+
+// - RouteHandler, ServerActions
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { Database } from "@/types/supabase";
+import { NextRequest, NextResponse } from "next/server";
+import { getCookie, setCookie } from "cookies-next";
+
+export const createServerSideClient = async(serverComponent = false) => {
+  const cookieStore = cookies();
+
+  return createServerClient<Database> (
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => cookieStore.get(key)?.value,
+        set: (key, value, options) => {
+          if (serverComponent) return;
+          cookieStore.set(key, value, options);
+        },
+        remove: (key, options) => {
+          cookieStore.set(key, "", options)
+        }
+      }
+    }
+  );
+};
+
+// - RSC
+export const createServerSideClientRSC = async() => {
+  return createServerSideClient(true);
+}
+
+// - MiddleWare
+export const createServerSideMiddleware = async(req: NextRequest, res: NextResponse) => {
+  return createServerClient<Database> (
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => getCookie(key, { req, res }),
+        set: (key, value, options) => {
+          setCookie(key, value, { req, res, ...options});
+        },
+        remove: (key, options) => {
+          setCookie(key, "", { req, res, ...options})
+        }
+      }
+    }
+  );
+
+}
